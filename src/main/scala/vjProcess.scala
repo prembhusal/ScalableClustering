d740b0abe41cb2b82057bec12e56ead97f57f1dc
prembhusal <remp.bhusal@gmail.com>
2019-03-10 16:13:56 -0400

    Update vjProcess.scala

--- src/main/scala/vjProcess.scala
+++ src/main/scala/vjProcess.scala
@@ -9,9 +9,9 @@ object vjProcess extends App{
     var df = spark.read.option("sep", "\t").option("header", "true").csv("data/d1vjIndex.tsv")// reading stat file in dataframe
     val groups = df.select("V_CALL","J_CALL","JUNCTION_LENGTH","JUNCTION","Index").groupBy("V_CALL","J_CALL","JUNCTION_LENGTH");
     
-    //val records = df.count()
+    val records = df.count()
     //New dataframe which has columns IDs and JUNC_SEQS having v,j length group 
     val groupList = groups.agg(collect_list("Index").as("IDS"), collect_list("JUNCTION").as("JUNC_SEQS"))
-    groupList.show(10)
+    groupList.show(10) // shows only 10 entries
   }
 }
